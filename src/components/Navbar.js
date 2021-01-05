7ae48518269b7d8fe1a108bf38e5cf650c74a0c1
anshu gupta <anshu.snm6@gmail.com>
2021-01-06 03:34:00 +0530

    gh-pages

---
+++ src/components/Navbar.js
@@ -0,0 +1,101 @@
+import React from 'react';
+import { connect } from 'react-redux';
+import { Link } from 'react-router-dom';
+
+import { logoutUser } from '../actions/auth';
+import { searchUsers } from '../actions/search';
+
+class Navbar extends React.Component {
+  logOut = () => {
+    localStorage.removeItem('token');
+    this.props.dispatch(logoutUser());
+  };
+
+  handleSearch = (e) => {
+    const searchText = e.target.value;
+
+    this.props.dispatch(searchUsers(searchText));
+  };
+
+  render() {
+    const { auth, results } = this.props;
+    return (
+      <nav className="nav">
+        <div className="left-div">
+          <Link to="/">
+            <img
+              src="https://ninjasfiles.s3.amazonaws.com/0000000000003454.png"
+              alt="logo"
+            />
+          </Link>
+        </div>
+        <div className="search-container">
+          <img
+            className="search-icon"
+            src="https://image.flaticon.com/icons/svg/483/483356.svg"
+            alt="search-icon"
+          />
+          <input placeholder="Search" onChange={this.handleSearch} />
+
+          {results.length > 0 && (
+            <div className="search-results">
+              <ul>
+                {results.map((user) => (
+                  <li className="search-results-row" key={user._id}>
+                    <Link to={`/user/${user._id}`}>
+                      <img
+                        src="https://image.flaticon.com/icons/svg/2154/2154651.svg"
+                        alt="user-dp"
+                      />
+                      <span>{user.name}</span>
+                    </Link>
+                  </li>
+                ))}
+              </ul>
+            </div>
+          )}
+        </div>
+        <div className="right-nav">
+          {auth.isLoggedin && (
+            <div className="user">
+              <Link to="/settings">
+                <img
+                  src="https://image.flaticon.com/icons/svg/2154/2154651.svg"
+                  alt="user-dp"
+                  id="user-dp"
+                />
+              </Link>
+              <span>{auth.user.name}</span>
+            </div>
+          )}
+
+          <div className="nav-links">
+            <ul>
+              {!auth.isLoggedin && (
+                <li>
+                  <Link to="/login">Log in</Link>
+                </li>
+              )}
+
+              {auth.isLoggedin && <li onClick={this.logOut}>Log out</li>}
+
+              {!auth.isLoggedin && (
+                <li>
+                  <Link to="/signup">Register</Link>
+                </li>
+              )}
+            </ul>
+          </div>
+        </div>
+      </nav>
+    );
+  }
+}
+
+function mapStateToProps(state) {
+  return {
+    auth: state.auth,
+    results: state.search.results,
+  };
+}
+export default connect(mapStateToProps)(Navbar);
