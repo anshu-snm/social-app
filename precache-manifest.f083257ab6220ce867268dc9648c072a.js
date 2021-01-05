ced7e8c4679b40b1018e573255d75ef9e9738453
anshu gupta <anshu.snm6@gmail.com>
2021-01-06 03:45:32 +0530

    Updates

---
+++ precache-manifest.f083257ab6220ce867268dc9648c072a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f133556a209477b94137b2a740f6e48e",
+    "url": "/io/social-app/index.html"
+  },
+  {
+    "revision": "7d625bd0cf7f1174bea6",
+    "url": "/io/social-app/static/css/main.4990e117.chunk.css"
+  },
+  {
+    "revision": "a75c7f3e4de30c14bc25",
+    "url": "/io/social-app/static/js/2.c6bdd757.chunk.js"
+  },
+  {
+    "revision": "d0ed0e93fdba385200229b33bb0108ea",
+    "url": "/io/social-app/static/js/2.c6bdd757.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7d625bd0cf7f1174bea6",
+    "url": "/io/social-app/static/js/main.24de47e5.chunk.js"
+  },
+  {
+    "revision": "e26d2fa484343197f4fa",
+    "url": "/io/social-app/static/js/runtime-main.5e007263.js"
+  }
+]);
